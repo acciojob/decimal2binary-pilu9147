712b8c38c8d7a03d90e5c2d3d0631a849dfe6995
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-29 17:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function threeSum(arr, target) {
 // write your code here
-	arr.toString(target, 2);
+	let ans = "";
+	while(arr > 0) {
+		let rem = parseInt(arr%2);
+		ans.concat(rem.toString());
+		arr = parseInt(arr/2)
+	}
+	return ans;
   
 }
 
